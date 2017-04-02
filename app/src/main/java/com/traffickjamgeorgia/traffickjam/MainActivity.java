7a26b0ad5b252f3e0ba847fb4e4114ba25e40d79
propoxyphene13 <propoxyphene13@gmail.com>
2017-04-02 15:04:21 -0400

    began main page work

--- app/src/main/java/com/traffickjamgeorgia/traffickjam/MainActivity.java
+++ app/src/main/java/com/traffickjamgeorgia/traffickjam/MainActivity.java
@@ -2,7 +2,6 @@ package com.traffickjamgeorgia.traffickjam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
