2a220987207423eff342ac876ef49248a3acae12
propoxyphene13 <propoxyphene13@gmail.com>
2017-04-09 16:46:54 -0400

    started Contact page

--- app/src/main/java/com/traffickjamgeorgia/traffickjam/Contact.java
+++ app/src/main/java/com/traffickjamgeorgia/traffickjam/Contact.java
@@ -2,12 +2,26 @@ package com.traffickjamgeorgia.traffickjam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Contact extends AppCompatActivity {
+
+    //Insert contact methods...
+    // Text is for info
+    // Image buttons are for types like twitter, fb, instagram...
+    // hope to make these launch those respective apps
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
     }
+
+    public void onBtnClick(View v){
+
+    if(){
+
+    }
+
 }
