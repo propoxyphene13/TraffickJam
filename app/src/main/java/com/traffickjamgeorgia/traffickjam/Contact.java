2036e376c045d8f3986dc55e9373c424c727453e
propoxyphene13 <propoxyphene13@gmail.com>
2017-04-10 17:51:19 -0400

    buttons linked, need intents to start apps

--- app/src/main/java/com/traffickjamgeorgia/traffickjam/Contact.java
+++ app/src/main/java/com/traffickjamgeorgia/traffickjam/Contact.java
@@ -1,8 +1,10 @@
 package com.traffickjamgeorgia.traffickjam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Contact extends AppCompatActivity {
 
@@ -12,15 +14,42 @@ public class Contact extends AppCompatActivity {
     // hope to make these launch those respective apps
 
 
+
+    // Delete if not needed anymore:
+    // ImageButton fb;
+    // ImageButton tw;
+    // ImageButton ig;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+     //   Delete if not needed anymore:
+     //   fb=(ImageButton)findViewById(R.id.fb_btn);
+     //   tw=(ImageButton)findViewById(R.id.tw_btn);
+     //   ig=(ImageButton)findViewById(R.id.ig_btn);
+
     }
 
-    public void onBtnClick(View v){
+    public void onBtnClick(View v) {
 
-    if(){
+        // http://stackoverflow.com/questions/4810803/open-facebook-page-from-android-app
+
+        switch (v.getId()) {
+            case R.id.fb_btn:
+                startActivity(new Intent());
+                break;
+
+            case R.id.tw_btn:
+                startActivity(new Intent());
+                break;
+
+            case R.id.ig_btn:
+                startActivity(new Intent());
+                break;
+        }
 
     }
 
